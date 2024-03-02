d6f224832e83e3e7130ce8658e2d9bacb8839697
arvind-ishi <avij70507@gmail.com>
2024-03-02 09:09:12 +0530

    this is the new

---
+++ project2/script.js
@@ -0,0 +1,29 @@
+// const navbar = document.getElementById('navbar');
+
+// let scrollPosition = 0;
+
+// window.addEventListener("scroll",()=>{
+//     scrollPositon= window.pageYOffset;
+//     navbar.style.top = `-${scrollPosition}px`;
+
+// });
+
+
+// navbar.addEventListener("mouseover",()=>{
+
+//     window.addEventListener("scroll",()=>{
+//         scrollPosition= window.pageYOffset;
+//         navbar.style.top = `-${scrollPosition}px`
+
+//     });
+
+// });
+
+// navbar.addEventListener("mouseout",()=>{
+//     window.removeEventListener("scroll",()=>{
+
+//         scrollPosition=window.pageYOffset;
+//         navbar.style.top =`-${scrollPosition}px`
+
+//     });
+// });
